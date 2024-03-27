ea389d2a05e6d510ceca075999268be0fd9287c8
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-03-26 22:04:20 -0300

    Configuração do arquivo db.py, responsável por criar os objetos que serão utilizados para a persistência de dados da aplicação

---
+++ src/db.py
@@ -0,0 +1,19 @@
+#Este arquivo irá ficar responsável por importar dependência necessárias para 
+#a conexão com o banco de dados
+from sqlalchemy import create_engine;
+from sqlalchemy.orm import sessionmaker;
+from sqlalchemy.ext.declarative import declarative_base;
+from settings import STR_DATABASE;
+
+#Cria um mecanismo de conexão com o banco de dados
+engine = create_engine(STR_DATABASE, echo=True);
+
+#Criará as sessões que iremos utilizar nas nossas transações com o banco de dados
+Session = sessionmaker(bind=engine);
+
+#Irá ficar responsável por "representar" as entidades no banco de dados de acordo com o mapeamento ORM
+Base = declarative_base();
+
+# Função para criar as tabelas com base no mapeamento caso não existam no banco de dados
+def criaTabelas():
+    Base.metadata.create_all(engine);
