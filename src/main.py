2a1a168e9ad4ea611e390be1252a0022a6f5647b
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-03-27 19:18:17 -0300

    Classe main alterada para gerar as tabelas quando não existirem

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from mod_funcionario import FuncionarioDAO;
 from mod_cliente import ClienteDAO;
 from mod_produto import ProdutoDAO;
 
+import db;
+
 app = FastAPI();
 
 #Definindo um tratamento para a "rota padrão"
@@ -17,6 +19,9 @@ def root():
 app.include_router(FuncionarioDAO.router);
 app.include_router(ClienteDAO.router);
 app.include_router(ProdutoDAO.router);
+
+#Caso não exista, cria as tabelas no banco de dados
+db.criaTabelas();
 
 if __name__ == "__main__":
     import uvicorn
